619274f2e9fa07b569da9f006f91a4b56eb5f50e
Anatoliy Morozov <anatoly.morozov@radiogigabit.net>
2024-12-12 14:43:02 +0300

    Created StiffOdeModel

---
+++ StiffOdeModel.cpp
@@ -0,0 +1,74 @@
+#include "StiffOdeModel.hpp"
+#include <QObject>
+#include <QtCharts/QLineSeries>
+#include <vector>
+#include <functional>
+
+namespace StiffOde
+{
+StiffOdeModel::StiffOdeModel(QObject* parent)
+    : QObject(parent), m_startTime(0.0), m_endTime(0.0), m_stepSize(0.1)
+{
+}
+
+void StiffOdeModel::setSystem(const std::function<std::vector<double>(const std::vector<double>&, double)>& system)
+{
+    m_system = system;
+}
+
+void StiffOdeModel::setInitialConditions(const std::vector<double>& initialConditions, double startTime)
+{
+    m_initialConditions = initialConditions;
+    m_startTime = startTime;
+}
+
+void StiffOdeModel::setParameters(double stepSize, double endTime)
+{
+    m_stepSize = stepSize;
+    m_endTime = endTime;
+}
+
+void StiffOdeModel::solve()
+{
+    if (!m_system || m_initialConditions.empty())
+        return;
+
+    size_t numEquations = m_initialConditions.size();
+
+    for (auto* series : m_series) {
+        delete series;
+    }
+    m_series.clear();
+
+    for (size_t i = 0; i < numEquations; ++i) {
+        m_series.push_back(new QLineSeries(this));
+    }
+
+    std::vector<double> y = m_initialConditions;
+    double t = m_startTime;
+
+    while (t <= m_endTime) {
+        for (size_t i = 0; i < numEquations; ++i) {
+            m_series[i]->append(t, y[i]);
+        }
+
+        std::vector<double> yNext = y;
+        double tNext = t + m_stepSize;
+
+        for (int iteration = 0; iteration < 10; ++iteration) {
+            std::vector<double> fValue = m_system(yNext, tNext);
+            for (size_t i = 0; i < numEquations; ++i) {
+                yNext[i] = y[i] + m_stepSize * fValue[i];
+            }
+        }
+
+        y = yNext;
+        t = tNext;
+    }
+}
+
+const std::vector<QLineSeries*>& StiffOdeModel::getSeries() const
+{
+    return m_series;
+}
+}
---
+++ StiffOdeModel.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QObject>
+#include <QtCharts/QLineSeries>
+
+using namespace QtCharts;
+
+namespace StiffOde
+{
+class StiffOdeModel : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit StiffOdeModel(QObject* parent = nullptr);
+    void setSystem(const std::function<std::vector<double>(const std::vector<double>&, double)>& system);
+    void setInitialConditions(const std::vector<double>& initialConditions, double startTime);
+    void setParameters(double stepSize, double endTime);
+    void solve();
+    const std::vector<QLineSeries*>& getSeries() const;
+
+private:
+    std::function<std::vector<double>(const std::vector<double>&, double)> m_system;
+    std::vector<double> m_initialConditions;
+    double m_startTime;
+    double m_endTime;
+    double m_stepSize;
+    std::vector<QLineSeries*> m_series;
+};
+}
