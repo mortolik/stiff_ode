10b980674e5413969956cc94c255348ddd6c7d20
Anatoliy Morozov <anatoly.morozov@radiogigabit.net>
2024-12-12 18:09:15 +0300

    Added base in MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "StiffOdeModel.hpp"
+#include "StiffOdeWidget.hpp"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    StiffOde::StiffOdeModel* model = new StiffOde::StiffOdeModel(this);
+    model->setInitialConditions({7, 13}, 0.0);
+    model->setParameters(0.005, 0.1);
+    model->solve();
+
+    StiffOde::StiffOdeWidget* stiffOdeWidget = new StiffOde::StiffOdeWidget(model, this);
+
+    setCentralWidget(stiffOdeWidget);
 }
 
 MainWindow::~MainWindow()
